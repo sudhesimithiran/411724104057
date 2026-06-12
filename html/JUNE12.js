4700e409606e5b35b298c41483ba7949260bbdd2
unknown <sudheisd@gmail.com>
2026-06-12 11:32:46 +0530

    JS

---
+++ html/JUNE12.js
@@ -0,0 +1,43 @@
+console.log("external js")
+const a = 10;
+const b = 20;
+console.log(a+b)
+const str = "Ammu"
+console.log(str)
+
+const arr = [1,,2.1,"str",false]
+console.log(arr)
+
+arr[1]=3.1
+console.log(arr[1])
+
+arr.push(4)
+console.log(arr)
+
+
+arr. pop()
+console.log(arr)
+
+for (let i =0 ; i<arr.length ; i++){
+    console.log(arr[i])
+}
+
+function example(){
+    console.log("am writing something ")
+}
+
+
+const access = document.getElementById("ptag")
+access.innerHTML = "Paragarh"
+
+const input = document.getElementById("input")
+const output = document.getElementById("inputField")
+
+input.addEventListener("input", ()=>{
+    output.textContent = input.value
+})
+
+const first = document.createElement("p")
+first.textContent = "some txt"
+document.body.appendChild(first)first
+first.remove()
